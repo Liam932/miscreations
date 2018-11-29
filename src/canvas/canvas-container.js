32ab58da13dc5af88eabd4d392b911d51969c133
Liam Childs <liam.childs91@gmail.com>
2018-11-29 22:19:06 +0000

    Delete Canvas Container

--- src/canvas/canvas-container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import Canvas from './canvas';
-import { getMonsters } from '../monster/monster-redux';
-
-const mapStateToProps = state => ({
-    monsters: getMonsters(state)
-})
-
-export default connect(mapStateToProps)(Canvas)
