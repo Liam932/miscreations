7d2200b3d1c1c8ab784af373f3ce49d11d59e4a7
Liam Childs <liam.childs91@gmail.com>
2019-01-06 13:54:37 +0000

    Eye Animation

--- src/monster/monster-eyes.js
+++ src/monster/monster-eyes.js
@@ -1,30 +1,52 @@
-import React from 'react';
-import { Circle } from "react-konva";
+import React, { PureComponent } from 'react';
+import { Ellipse } from 'react-konva';
+import { Spring } from 'react-spring'
 
-const OUTER_RATIO = 20 / 70;
-const INNER_RATIO = 10 / 70;
+const OUTER_RATIO = 20 / 35;
+const INNER_RATIO = 10 / 35;
 
-const MonsterEyes = ({ genome, x, y, blink } = {}) => (
-    <>
-      <Circle
-        x={x}
-        y={y - 20}
-        radius={genome.bodySize * OUTER_RATIO}
-        fill={'#fff'}
-        stroke="black"
-        strokeWidth={4}
-      />
+class MonsterEyes extends PureComponent {
 
-      <Circle
-        x={x}
-        y={y - 20}
-        radius={genome.bodySize * INNER_RATIO}
-        fill={genome.eyeColour}
-        stroke="black"
-        strokeWidth={1}
-      />
-  </>
-);
+    constructor(props) {
+        super(props);
+        this.state = { reverse: false }
+    }
+
+    onRest = () => this.setState({ reverse: !this.state.reverse })
+
+    render() {
+        const { x, y, genome } = this.props;
+        return (
+        <Spring reset={true} reverse={this.state.reverse} from={{percentage: 1}} to={{percentage: 0}} onRest={this.onRest}>
+            {props => {
+                return (
+                    <>
+                      <Ellipse
+                        x={x}
+                        y={y - 20}
+                        height={ genome.bodySize * OUTER_RATIO }
+                        width= { genome.bodySize * OUTER_RATIO }
+                        fill={'#fff'}
+                        stroke="black"
+                        strokeWidth={4}
+                      />
+
+                      <Ellipse
+                        x={x}
+                        y={y - 20}
+                        height={ genome.bodySize * INNER_RATIO * props.percentage }
+                        width= { genome.bodySize * INNER_RATIO }
+                        fill={genome.eyeColour}
+                        stroke="black"
+                        strokeWidth={1}
+                      />
+                    </>
+                )
+            }}
+      </Spring>
+    )
+    }
+}
 
 
 export default MonsterEyes
